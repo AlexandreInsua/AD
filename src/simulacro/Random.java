8ecb5183d0408ee8cc29a7a994adf8cc57297e66
Alexandre Insua <alexandreinsua@gmail.com>
2018-02-15 08:36:28 +0100

    Clase ramdon

---
+++ src/simulacro/Random.java
@@ -0,0 +1,9 @@
+package simulacro;
+
+public class Random {
+public static void main(String[] args) {
+	
+	char letra =   (char) (Math.random()*(90-65)+65);
+	System.out.println(letra);
+}
+}
---
+++ src/simulacro/Random2.java
@@ -0,0 +1,7 @@
+package simulacro;
+
+public class Random2 {
+public static void main(String[] args) {
+	System.out.println("H");
+}
+}
